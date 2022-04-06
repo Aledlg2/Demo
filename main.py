8db11b27e6181a1924445006e7b6a3ab7ca21f75
nitsuga-vr <a01233454@tec.mx>
2022-04-06 18:33:30 -0500

    v1

---
+++ main.py
@@ -0,0 +1,12 @@
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+
+url= "https://www.cyberpuerta.mx/Audio-y-Video/Audio-y-MP3/Audifonos/1/"
+page= requests.get(url)
+soup= BeautifulSoup(page.content, 'html.parser')
+
+#Contenedores
+#title= soup.find_all('div', class_= 'emproduct_right')
+containers= soup.find_all('div', {'class':'emproduct clear listitemline emsmoothhover2'})
+print(containers[0])

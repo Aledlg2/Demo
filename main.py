558dbd5067225bb30f78829d9aac0d2215ca4587
nitsuga-vr <a01233454@tec.mx>
2022-04-13 18:56:01 -0500

    v2

--- main.py
+++ main.py
@@ -2,12 +2,21 @@ from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 
-url= "https://www.cyberpuerta.mx/Audio-y-Video/Audio-y-MP3/Audifonos/1/"
-page= requests.get(url)
-soup= BeautifulSoup(page.content, 'html.parser')
+url = "https://www.cyberpuerta.mx/Audio-y-Video/Audio-y-MP3/Audifonos/1/"
+page = requests.get(url)
+soup = BeautifulSoup(page.content, 'html.parser')
 
-#Contenedores
-#title= soup.find_all('div', class_= 'emproduct_right')
-containers= soup.find_all('div', {'class':'emproduct clear listitemline emsmoothhover2'})
-print(containers[0])
-#gitdemo
+containers = soup.find_all('div', {'class': 'emproduct clear listitemline emsmoothhover2'})
+
+filename = "Audifonos.csv"
+f = open(filename, "w")
+
+headers = "Marca\n"
+
+f.write(headers)
+
+
+for container in containers:
+    marca_container = container.findAll("div", {"class": "badges-left"})
+    marca = marca_container[0].img["alt"].title()
+    f.write(marca + "\n")
